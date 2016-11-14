16175b2d0fc4a4936efaf3173532adc4666786f0
Leonard <leonardquintal@gmail.com>
2016-11-14 13:16:18 -0500

    .

--- Assets/_Framework/lightCamera.cs
+++ Assets/_Framework/lightCamera.cs
@@ -16,12 +16,18 @@ public class lightCamera : MonoBehaviour {
 
     public bool isAmbiant = false;
     public float isOnDuration = 2f;
+
+    private Transform m_mainObj;
+    private float m_scale = 0.01f;
 	// Use this for initialization
 	void Start () {
         spotLight = transform.parent.transform.FindChild("Triangle").GetComponent<Renderer>().material;
+        m_mainObj = transform.parent;
 
+       
         if (isAmbiant)
         {
+            m_scale = 1f;
             spotLight.color = new Vector4(1f, 1f, 1f, Random.Range(intensityRange.x, intensityRange.y));
         }
         else
@@ -32,7 +38,7 @@ public class lightCamera : MonoBehaviour {
             target = p.transform;
             pScript = p.GetComponent<player>();
         }
-
+        m_mainObj.localScale = new Vector3(m_scale, 1f, 1f);
 
     }
 	
@@ -43,26 +49,20 @@ public class lightCamera : MonoBehaviour {
        
         if (isAmbiant)
         {
-            Fluctuate();
             return;
         }
         Vector3 screenPoint = spot.WorldToViewportPoint(target.position);
         bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
 
-        if (onScreen && spotLight.color.a >= 0.2f)
+        if (onScreen)
         {
-            pScript.Photosynthesize(spotLight.color.a * Time.deltaTime);
+            pScript.Photosynthesize(1f * Time.deltaTime);
         }
         else
         {
             pScript.Photosynthesize(false);
         }
 
-        if (target.position.y + 10f >= transform.position.y)
-        {
-            FadingFromPlayer();
-            return;
-        }
         if (isOff)
         {
             return;
@@ -83,7 +83,7 @@ public class lightCamera : MonoBehaviour {
 
     void Fluctuate()
     {
-        if (spotLight.color.a >= intensityRange.y)
+        if (m_scale >= intensityRange.y)
         {
             goingBrighter = false;
             isOff = true;
@@ -96,7 +96,7 @@ public class lightCamera : MonoBehaviour {
                 StartCoroutine(LeaveOn(isOnDuration));
             }
         }
-        if (spotLight.color.a <= intensityRange.x)
+        if (m_scale <= intensityRange.x)
         {
             if (!isAmbiant)
             {
@@ -112,12 +112,52 @@ public class lightCamera : MonoBehaviour {
         }
         if (goingBrighter)
         {
-            spotLight.color = new Vector4(1f, 1f, 1f, spotLight.color.a + (rateOfFlux * Time.deltaTime));
+            m_scale += rateOfFlux * Time.deltaTime;
+            m_mainObj.localScale = new Vector3(m_scale, 1f, 1f);
         }
         else
         {
-            spotLight.color = new Vector4(1f, 1f, 1f, spotLight.color.a - (rateOfFlux * Time.deltaTime));
+            m_scale -= rateOfFlux * Time.deltaTime;
+            m_mainObj.localScale = new Vector3(m_scale, 1f, 1f);
         }
+
+        //if (spotLight.color.a >= intensityRange.y)
+        //{
+        //    goingBrighter = false;
+        //    isOff = true;
+        //    if (isAmbiant)
+        //    {
+        //        StartCoroutine(LeaveOn(Random.Range(0f, 2f)));
+        //    }
+        //    else
+        //    {
+        //        StartCoroutine(LeaveOn(isOnDuration));
+        //    }
+        //}
+        //if (spotLight.color.a <= intensityRange.x)
+        //{
+        //    if (!isAmbiant)
+        //    {
+        //        Destroy(transform.parent.gameObject);
+        //    }
+        //    else
+        //    {
+        //        goingBrighter = true;
+        //        isOff = true;
+        //        StartCoroutine(LeaveOff(Random.Range(5f, 10f)));
+        //    }
+
+        //}
+        //if (goingBrighter)
+        //{
+        //    spotLight.color = new Vector4(1f, 1f, 1f, spotLight.color.a + (rateOfFlux * Time.deltaTime));
+        //}
+        //else
+        //{
+        //    spotLight.color = new Vector4(1f, 1f, 1f, spotLight.color.a - (rateOfFlux * Time.deltaTime));
+        //}
+
+
     }
 
 
